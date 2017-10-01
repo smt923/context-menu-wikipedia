4f357065be4988cb47530a06283f9ccf0b0f8894
smt923 <smtea923@gmail.com>
2017-10-01 16:23:08 +0100

    move selection text to its own var

--- bg.js
+++ bg.js
@@ -5,10 +5,11 @@ browser.contextMenus.create({
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
+  var searchtext = info.selectionText.trim();
   switch (info.menuItemId) {
     case "wiki-search":
       var opentab = browser.tabs.create({
-        url: "https://wikipedia.org/w/index.php?search="+info.selectionText.trim()
+        url: "https://wikipedia.org/w/index.php?search="+searchtext
       });
       opentab.then(onError);
       break;
